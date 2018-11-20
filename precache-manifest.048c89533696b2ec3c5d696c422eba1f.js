2b143638be76b1a5b75f4745ec0ae7042a431aee
Eric <eric.saintetienne@gmail.com>
2018-11-20 18:57:38 +0000

    gitignore `package-lock.json` RAZ more dynamic, fix tap on header column
    Sync with gitlab ed9d05a0b630cf8da3de35178ef64d4706b77303

---
+++ precache-manifest.048c89533696b2ec3c5d696c422eba1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb29782f1d494e528cb6",
+    "url": "/yams/static/js/runtime~main.bb29782f.js"
+  },
+  {
+    "revision": "9290a9b13bcf19037005",
+    "url": "/yams/static/js/main.9290a9b1.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/yams/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "9290a9b13bcf19037005",
+    "url": "/yams/static/css/main.a9b07f02.chunk.css"
+  },
+  {
+    "revision": "8bfc03205093fdc2228d989243f73dc4",
+    "url": "/yams/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/yams/precache-manifest.48dcafde2ed78412d5907d7518d1b50f.js"
+  "/yams/precache-manifest.048c89533696b2ec3c5d696c422eba1f.js"
 );
 
 workbox.clientsClaim();
